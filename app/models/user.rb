8a0d580e134bc83cdf9734e68bbe80a4a1eb2764
matsu-sy <k.c.e.38m@gmail.com>
2026-01-17 23:06:15 +0900

    test,lint修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  enum hometown_visibility: { hidden: 0, visible: 1 }
+  enum :hometown_visibility, { hidden: 0, visible: 1 }
 end
