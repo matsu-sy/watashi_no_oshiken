bd319962aa3529efb028f99bb5faed92a2a9f39d
matsu-sy <k.c.e.38m@gmail.com>
2026-02-08 18:23:21 +0900

    feat/マイページにリアクション一覧取得のコントローラーを追加

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -4,5 +4,21 @@ class MypagesController < ApplicationController
   def show
     @user = current_user
     @hometowns = current_user.hometowns
-  end
+
+    reacted_ids =
+      current_user.reactions
+        .select("post_id, MAX(created_at) AS reacted_at")
+        .group(:post_id)
+        .order("reacted_at DESC")
+        .pluck(:post_id)
+
+    @reacted_posts =
+      if reacted_ids.empty?
+        Post.none
+      else
+        Post
+          .includes(:user, :category, :reactions)
+          .where(id: reacted_ids)
+          .order(Arel.sql("array_position(ARRAY[#{reacted_ids.join(',')}], posts.id)"))
+      end
 end
