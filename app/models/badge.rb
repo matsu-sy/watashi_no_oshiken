82efa8d9df48f95d4c2633f21878bbff012f65f0
matsu-sy <k.c.e.38m@gmail.com>
2026-03-25 06:21:57 +0900

    feat/Badgeモデルと複合ユニーク制約を追加

---
+++ app/models/badge.rb
@@ -0,0 +1,9 @@
+class Badge < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+
+  enum :badge_type, { star: 0 }
+
+  validates :badge_type, presence: true
+  validates :post_id, uniqueness: { scope: [:user_id, :badge_type] }
+end
---
+++ db/migrate/20260324211119_create_badges.rb
@@ -0,0 +1,13 @@
+class CreateBadges < ActiveRecord::Migration[7.2]
+  def change
+    create_table :badges do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :post, null: false, foreign_key: true
+      t.integer :badge_type, null: false
+
+      t.timestamps
+    end
+
+    add_index :badges, [:user_id, :post_id, :badge_type], unique: true, name: 'index_badges_on_user_post_type'
+  end
+end
